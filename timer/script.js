b8a22819e93cec80b5e08c7fbba4057eef3c226b
Hayley <bra22025@byui.edu>
2024-10-26 17:28:47 -0600

    added short/long break to timer

--- timer/script.js
+++ timer/script.js
@@ -14,10 +14,21 @@ document.addEventListener("DOMContentLoaded", function () {
         return workMinutes * 60; //converts minutes to seconds
     }
 
+    function getShortBreakTimeInSeconds() {
+        const shortBreakMinutes = parseInt(document.getElementById('short').value, 10); //gets time from the form
+        return shortBreakMinutes * 60; //converts minutes to seconds
+    }
+    
+    function getLongBreakTimeInSeconds() {
+        const longBreakMinutes = parseInt(document.getElementById('long').value, 10); //gets time from the form
+        return longBreakMinutes * 60; //converts minutes to seconds
+    }
+
     let timerInterval;
     let timeLeft;
+    let workSessions = 0;
 
-    //Changes time displayed on timer
+    //Changes time displayed on timer for work sessions
     function startTimer(){
         clearInterval(timerInterval);
         timeLeft = getWorkTimeInSeconds();
@@ -27,14 +38,57 @@ document.addEventListener("DOMContentLoaded", function () {
                 timeLeft--; //subtract the amount of time every second
                 timerDisplay.textContent = formatTime(timeLeft); //calls the formatTime function to change the value of timerDisplay
             } else {
-                clearInterval(timerInterval); //replace the timer with a message once the time is up
-                timerDisplay.textContent = "Time's up!";
+                clearInterval(timerInterval);
+                workSessions++;
+                if (workSessions % 4 === 0)
+                {
+                    startLongBreak();
+                }
+                else
+                {
+                    startShortBreak();
+                }
             }
         }
         timerInterval = setInterval(updateTimer, 1000); //update every second
     }
 
+    function startShortBreak() {
+        timeLeft = getShortBreakTimeInSeconds();
+        timerDisplay.textContent = formatTime(timeLeft);
 
+        function updateTimer() {
+            if (timeLeft > 0) {
+                timeLeft--;
+                timerDisplay.textContent = formatTime(timeLeft)
+            }
+            else {
+                startTimer();
+            }
+        }
+        timerInterval = setInterval(updateTimer, 1000)
+    }
+
+    function startLongBreak() {
+        timeLeft = getLongBreakTimeInSeconds();
+        timerDisplay.textContent = formatTime(timeLeft);
+
+        function updateTimer() {
+            if (timeLeft > 0) {
+                timeLeft--;
+                timerDisplay.textContent = formatTime(timeLeft)
+            }
+            else {
+                startTimer()
+            }
+        }
+        timerInterval = setInterval(updateTimer, 1000)
+    }
+
+    // TODO allow user to exit timer
+    // TODO allow user to reset timer
+    // TODO display how many work sessions have been completed
+    // TODO display how many full Pomo-sessions have been completed?
 
     document.getElementById("start-button").addEventListener("click", startTimer);
 });
