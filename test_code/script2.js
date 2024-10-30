92859159e640c82b0f00eba3371e146ce742bf98
calebhpacker <packercaleb.h@gmail.com>
2024-10-30 14:45:00 -0600

    feed button

--- test_code/script2.js
+++ test_code/script2.js
@@ -1,20 +1,20 @@
 const imageElement = document.getElementById("animated");
 
-const images = 
-[
-    "C:/Users/fatei/.vscode/cse310 team/Pomodoro-Tamagotchi-main/test_code/pet/dogimage1.png",
-    "C:/Users/fatei/.vscode/cse310 team/Pomodoro-Tamagotchi-main/test_code/pet/dogimage2.png",
-    "C:/Users/fatei/.vscode/cse310 team/Pomodoro-Tamagotchi-main/test_code/pet/dogimage3.png",
-    "C:/Users/fatei/.vscode/cse310 team/Pomodoro-Tamagotchi-main/test_code/pet/dogimage4.png",
-];
-
-// const images =
+// const images = 
 // [
-//     "test_code/pet/dogimage1.png",
-//     "test_code/pet/dogimage2.png",
-//     "test_code/pet/dogimage3.png",
-//     "test_code/pet/dogimage4.png",
+//     "C:/Users/fatei/.vscode/cse310 team/Pomodoro-Tamagotchi-main/test_code/pet/dogimage1.png",
+//     "C:/Users/fatei/.vscode/cse310 team/Pomodoro-Tamagotchi-main/test_code/pet/dogimage2.png",
+//     "C:/Users/fatei/.vscode/cse310 team/Pomodoro-Tamagotchi-main/test_code/pet/dogimage3.png",
+//     "C:/Users/fatei/.vscode/cse310 team/Pomodoro-Tamagotchi-main/test_code/pet/dogimage4.png",
 // ];
+
+const images =
+[
+    "test_code/pet/dogimage1.png",
+    "test_code/pet/dogimage2.png",
+    "test_code/pet/dogimage3.png",
+    "test_code/pet/dogimage4.png",
+];
 
 let currIndex = 0;
 
