cb3df6f059b8b1b33ad9bb894cce1eb35dfdf34b
calebhpacker <packercaleb.h@gmail.com>
2024-11-06 14:41:50 -0700

    timer stuff

--- timer/script.js
+++ timer/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const hours = Math.floor(seconds / 3600);
         const minutes = Math.floor((seconds % 3600) / 60)+hours*60;
         const secs = seconds % 60;
-        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+        return `${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
     }
 
     function getTimeInSeconds(id) {
