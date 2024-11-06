235c1aded6ded47401ae4509ff8a418b7e768caa
calebhpacker <packercaleb.h@gmail.com>
2024-11-06 14:40:45 -0700

    fixed timer

--- timer/script.js
+++ timer/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     //Returns a string with the time formated like a digital clock
     function formatTime(seconds) {
         const hours = Math.floor(seconds / 3600);
-        const minutes = Math.floor((seconds % 3600) / 60);
+        const minutes = Math.floor((seconds % 3600) / 60)+hours*60;
         const secs = seconds % 60;
         return `${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
     }
