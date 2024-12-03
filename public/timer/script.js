0580763a84f82b7f6c764d70f6e3da9194736c38
Hayley <bra22025@byui.edu>
2024-12-02 17:59:53 -0700

    added exit button to timer

--- public/timer/script.js
+++ public/timer/script.js
@@ -101,7 +101,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 pomoCompleted.textContent = pomoSessions;
                 flow = "You just completed one session!";
                 flowCounter.textContent = flow;
-                flow = "⬤";
                 pet.addFood()
                 startTimer()
             }
@@ -109,15 +108,20 @@ document.addEventListener("DOMContentLoaded", function () {
         timerInterval = setInterval(updateTimer, 1000)
     }
 
-    // Allow user to exit timer
-    const exitButton = document.getElementById('exit-button');
-    exitButton.addEventListener('click', function() {
+    // Allow user to finish timer
+    const finishButton = document.getElementById('finished-button');
+    finishButton.addEventListener('click', function() {
         timeLeft = 0;});
     // Allow user to reset timer
     const resetButton = document.getElementById('reset-button');
     resetButton.addEventListener('click', function() {
         flow = "⬤";
         startTimer();});
+    // Allow user to end timer
+    const exitButton = document.getElementById('exit-button')
+    exitButton. addEventListener('click', function() {
+        location.reload()
+    });
 
     document.getElementById("start-button").addEventListener("click", startTimer);
 });
