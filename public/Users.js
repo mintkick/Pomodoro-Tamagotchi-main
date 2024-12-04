0e05fc4252642e2788fcbbef7fb950db83b626df
eeshurtliff <eeshurtliff@gmail.com>
2024-12-03 20:03:43 -0700

    update users.js

--- public/Users.js
+++ public/Users.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
+import { Schema, model } from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   userID: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
 });
 
-const User = mongoose.model("User", userSchema);
+const User = model("User", userSchema);
 
-module.exports = User;
+export default User;
