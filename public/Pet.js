76f4af7e2dc1937b7f525058b85c50cc4b33efdb
mintkick <adamx246@yahoo.com>
2024-12-08 17:26:33 -0700

    added default name (easter egg); added emotion animation changing based on user's interaction

--- public/Pet.js
+++ public/Pet.js
@@ -1,105 +1,119 @@
-import petBusiness from '../pet.business.js'
-import { setEmotion } from "./pet_anim_canvas.js";
+import petBusiness from "../pet.business.js";
 class Pet {
-    constructor(name) {
-      var userPet = petBusiness.getPet()
-      if (userPet){
-        this.name = userPet.name
-        this.satiated = userPet.satiated
-        this.food = userPet.food
-      }else{
-        this.name = name || 'no name';
-        this.satiated = 50; 
-        this.food = 50; //change to 0 
-        
-        userPet = {
-          name: this.name,
-          satiated: this.satiated,
-          food: this.food
-        }
-        petBusiness.createPet(userPet)  
-
+  constructor(name) {
+    var userPet = petBusiness.getPet();
+    if (userPet) {
+      if (userPet.name == null) {
+        this.name = "MISSINGNO.";
+      } else {
+        this.name = userPet.name;
       }
-    
-  
-  
-      const savedState = JSON.parse(localStorage.getItem('petState'));
-      if (savedState) {
-        this.satiated = savedState.satiated;
-        this.food = savedState.food;
-      }
-  
-      this.feedPet = this.feedPet.bind(this);
-      this.showStatus = this.showStatus.bind(this);
-      this.addFood = this.addFood.bind(this);
-      this.playWithPet = this.playWithPet.bind(this);
-      
-      document.addEventListener('DOMContentLoaded', () => {
-        this.initEventListeners();
-      });
+      this.satiated = userPet.satiated;
+      this.food = userPet.food;
+      if (Math.random() > 0.45) setEmotion("sleepy"); // random chance that the pre-existing pet starts asleep
+    } else {
+      this.name = name || "MISSINGNO.";
+      this.satiated = 50;
+      this.food = 50; //change to 0
 
-  
-      Pet.instance = this; 
-      return this;
-    }
-  
-    saveState() {
-      const state = {
+      userPet = {
+        name: this.name,
         satiated: this.satiated,
         food: this.food,
       };
-      document.getElementById("displayFood").textContent = this.food;
-      document.getElementById("displaySat").textContent = this.satiated;
-      localStorage.setItem('petState', JSON.stringify(state));
+      petBusiness.createPet(userPet);
     }
-  
-    feedPet() {
-      if (this.food > 0) {
-        this.food--;
-        this.satiated += 10;
-        if (this.satiated > 100) this.satiated = 100;
-        this.updateStatus(`${this.name} has been fed! Satiation is now ${this.satiated}.`);
-        this.saveState();
-      } else {
-        this.updateStatus(`No food left to feed ${this.name}!`);
-      }
+
+    const savedState = JSON.parse(localStorage.getItem("petState"));
+    if (savedState) {
+      this.satiated = savedState.satiated;
+      this.food = savedState.food;
     }
-  
-    showStatus() {
-      this.updateStatus(`${this.name}'s satiation: ${this.satiated}<br>Food supply: ${this.food}`);
-    }
-  
-    addFood() {
-      this.food += 1;
-      console.log(`You added 1 bowl of feed. Total bowls: ${this.food}`);
+
+    this.feedPet = this.feedPet.bind(this);
+    this.showStatus = this.showStatus.bind(this);
+    this.addFood = this.addFood.bind(this);
+    this.playWithPet = this.playWithPet.bind(this);
+
+    document.addEventListener("DOMContentLoaded", () => {
+      this.initEventListeners();
+    });
+
+    Pet.instance = this;
+    return this;
+  }
+
+  saveState() {
+    const state = {
+      satiated: this.satiated,
+      food: this.food,
+    };
+    document.getElementById("displayFood").textContent = this.food;
+    document.getElementById("displaySat").textContent = this.satiated;
+    localStorage.setItem("petState", JSON.stringify(state));
+  }
+
+  feedPet() {
+    if (this.food > 0) {
+      this.food--;
+      this.satiated += 10;
+      if (this.satiated > 100) this.satiated = 100;
+      this.updateStatus(
+        `${this.name} has been fed! Satiation is now ${this.satiated}.`
+      );
       this.saveState();
-    }
-  
-    playWithPet() {
-      if (this.satiated > 10) {
-        this.satiated -= 10;
-        this.updateStatus(`${this.name} enjoyed playing! Satiation decreased to ${this.satiated}.`);
-        this.saveState();
-      } else {
-        this.updateStatus(`${this.name} is too hungry to play!`);
-      }
-    }
-  
-    getName() {
-      return this.name;  
-    }
-  
-    updateStatus(message) {
-      const statusDiv = document.getElementById('status');
-      statusDiv.innerHTML = message;
-    }
-  
-    initEventListeners() {
-      document.getElementById('feedBtn').addEventListener('click', this.feedPet);
-      document.getElementById('statusBtn').addEventListener('click', this.showStatus);
-      document.getElementById('playBtn').addEventListener('click', this.playWithPet);
+      setEmotion("idle"); // idle bc why not
+    } else {
+      this.updateStatus(`No food left to feed ${this.name}!`);
+      setEmotion("sad"); // sad bc no food
     }
   }
-  
-  const pet = new Pet('Roxy Biloxi the Pomodoro Tomogotchi'); //change to create new instance of Pet class with the name given by the user
-  export default pet;
+
+  showStatus() {
+    this.updateStatus(
+      `${this.name}'s satiation: ${this.satiated}<br>Food supply: ${this.food}`
+    );
+  }
+
+  addFood() {
+    this.food += 1;
+    console.log(`You added 1 bowl of feed. Total bowls: ${this.food}`);
+    this.saveState();
+  }
+
+  playWithPet() {
+    if (this.satiated > 10) {
+      this.satiated -= 10;
+      this.updateStatus(
+        `${this.name} enjoyed playing! Satiation decreased to ${this.satiated}.`
+      );
+      setEmotion("happy"); // happy bc played with
+      this.saveState();
+    } else {
+      this.updateStatus(`${this.name} is too hungry to play!`);
+      setEmotion("sleepy"); // sleepy bc no energy bc hungry
+    }
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  updateStatus(message) {
+    const statusDiv = document.getElementById("status");
+    statusDiv.innerHTML = message;
+  }
+
+  initEventListeners() {
+    document.getElementById("feedBtn").addEventListener("click", this.feedPet);
+    document
+      .getElementById("statusBtn")
+      .addEventListener("click", this.showStatus);
+    document
+      .getElementById("playBtn")
+      .addEventListener("click", this.playWithPet);
+  }
+}
+
+const pet = new Pet("Roxy Biloxi the Pomodoro Tomogotchi"); //change to create new instance of Pet class with the name given by the user
+export default pet;
